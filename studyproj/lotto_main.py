bc028528fa058da7e8727f71f01b2b5bfd14b2d5
swkim <dkksw16@gmail.com>
2018-02-21 21:55:26 +0900

    로또 번호 수정

--- studyproj/lotto_main.py
+++ studyproj/lotto_main.py
@@ -1,54 +1,39 @@
 #-*- coding: utf-8 -*-
 
-import configparser
-import logging.handlers
-import os
 import random
 
-logdir = '{0}\\log'.format(os.getcwd())
-
-if not os.path.exists(logdir):
-    os.makedirs(logdir)
-
-logger = logging.getLogger('auto_task_log')
-fileHandler = logging.FileHandler('{0}/autotask.log'.format(logdir))
-streamHandler = logging.StreamHandler()
-
-logger.addHandler(fileHandler)
-logger.addHandler(streamHandler)
-logger.setLevel(logging.DEBUG)
-
-
+#로또 번호 5개를 생성하는데, 45개의 숫자가 중복되지 않은 숫자로 5게임을 만든다.
 if __name__ == "__main__":
-    logger.info('AUTO TASK - Lotto645 Create')
+    #지난주 당첨 번호는 제외 하자 아니면 그냥 맘에 안드는 숫자라든가..
+    except_number = [6, 7, 18, 19, 30, 38]
+
+    lotto_base = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                 1, 1, 1, 1, 1]
 
 
-    config = configparser.ConfigParser()
-    if os.path.exists('.\config.txt'):
-        config.read('.\config.txt')
 
-    #45개의 숫자 중에서 중복되지 않는 30개의 숫자로 로또 5게임을 만든다.
-    lotto_numbers = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-                     1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-                     1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-                     1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-                     1, 1, 1, 1, 1]
-    lotto_games = []
-    lotto_single = []
+    #지난주 당첨 번호를 제외 한다.
+    for lott_number in except_number:
+        lotto_base[lott_number] = 0
 
-    while len(lotto_games) !=5 :
+    game_result = []
 
+    #랜덤한 숫자를 생성 꺼내서 게임결과에 넣어준다.
+    game_single = []
+    while len(game_result) != 5 :
         random_number = random.randrange(1, 45)
-        if lotto_numbers[random_number] == 1 :
-            lotto_numbers[random_number] = 0
-            lotto_single.append(random_number)
-            if len(lotto_single) == 6:
-                lotto_single.sort()
-                lotto_games.append(lotto_single)
-                lotto_single = []
+        if lotto_base[random_number] == 1 :
+            lotto_base[random_number] = 0
+            game_single.append(random_number)
+            if len(game_single) == 6 :
+                game_single.sort()
+                game_result.append(game_single)
+                game_single = []
 
+    for mygame in game_result:
+        print ("%17s" % (mygame))
 
-
-    for lotto in lotto_games:
-        print (lotto)
-
+    print(random)
